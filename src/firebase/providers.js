d3913bbe7cf293a0a7091f82e488d9cb7a10dbd4
Gustavo <gar.santeliz@gmail.com>
2022-07-17 13:52:38 -0400

    query created to create new user in firebase

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,9 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
@@ -6,7 +11,7 @@ const googleProvider = new GoogleAuthProvider();
 export const signInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(FirebaseAuth, googleProvider);
-    const credentials = GoogleAuthProvider.credentialFromResult(result);
+    // const credentials = GoogleAuthProvider.credentialFromResult(result);
 
     const { displayName, email, photoURL, uid } = result.user;
 
@@ -19,21 +24,44 @@ export const signInWithGoogle = async () => {
       photoURL,
       uid,
     };
-
   } catch (error) {
     console.log(error);
 
     // Handle Errors here.
-    const errorCode = error.code;
+    // const errorCode = error.code;
     const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
 
     return {
       ok: false,
-      errorMessage
+      errorMessage,
+    };
+  }
+};
+
+export const registerUserWithEmailPassword = async ({email, password, displayName }) => {
+
+
+  try {
+    const response = await createUserWithEmailAndPassword( FirebaseAuth, email, password );
+
+    const { uid, photoURL } = response.user;
+
+    //TODO: update displayName
+
+    await updateProfile( FirebaseAuth.currentUser, {displayName})
+
+    return {
+      ok: true,
+      uid, photoURL, email, displayName
+    }
+
+
+  } catch (error) {
+
+    console.log(error);
+    return {
+      ok: false,
+      errorMessage: error.message,
     };
   }
 };
