35e702967e609adbca99ae31a5c2ab5d7888324f
Gustavo <gar.santeliz@gmail.com>
2022-07-17 13:50:37 -0400

    creating funtion to create new user and push it to firebase and update the displayName

--- src/app/auth/thunk.js
+++ src/app/auth/thunk.js
@@ -1,4 +1,4 @@
-import { signInWithGoogle } from '../../firebase/providers'
+import { registerUserWithEmailPassword, signInWithGoogle } from '../../firebase/providers'
 import { checkingCredentials, login, logout} from './authSlice'
 
 export const checkingAuthentication = () =>{
@@ -19,4 +19,19 @@ export const startGoogleSignin = () =>{
         dispatch( login( result ))
 
     }
+}
+
+export const startCreatingUserWithEmailPassword = ({ email, password, displayName}) =>{
+    return async ( dispatch) =>{
+
+        dispatch( checkingCredentials() );
+
+       const { ok, uid, photoURL, errorMessage } =  await registerUserWithEmailPassword({ email, password, displayName })
+
+
+        if (!ok ) return dispatch( logout({ errorMessage }))
+
+        dispatch( login({ uid, displayName, email, photoURL }))
+    }
+
 }
