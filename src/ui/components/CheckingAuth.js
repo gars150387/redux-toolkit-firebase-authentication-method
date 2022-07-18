6ae9128cf50f54ec9cfa0d56c2e5690066973290
Gustavo <gar.santeliz@gmail.com>
2022-07-17 21:21:45 -0400

    ui section created to show user when data is loading

---
+++ src/ui/components/CheckingAuth.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CheckingAuth = () => {
+  return (
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  );
+};
