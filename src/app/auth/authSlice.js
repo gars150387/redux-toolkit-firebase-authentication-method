b529d6c9c8f051b95a4fbba85f22ac00643b2308
Gustavo <gar.santeliz@gmail.com>
2022-07-17 21:10:05 -0400

    logout errorMessage condition added if payload has an error message

--- src/app/auth/authSlice.js
+++ src/app/auth/authSlice.js
@@ -25,7 +25,7 @@ const authSlice = createSlice({
       state.email = null;
       state.photoURL = null;
       state.displayName = null;
-      state.errorMessage = payload.errorMessage;
+      state.errorMessage = payload?.errorMessage;
     },
     checkingCredentials: (state) => {
       state.status = "checking";
