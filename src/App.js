59d83aa4d1fa3bb2824ce2eef684a3c4da86a6a0
Gustavo <gar.santeliz@gmail.com>
2022-07-17 21:22:30 -0400

    routes conditioned and ui component added to page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { useCheckAuth } from "./hooks/useCheckAuth";
 
 import { Help } from "./pages/Help";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
+import { CheckingAuth } from "./ui/components/CheckingAuth";
+
 
 function App() {
+
+  const  status  = useCheckAuth()
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
+
   return (
     <div className="App">
-        <header className="App-header">
-          <Navbar />
-          <Routes>
+      <header className="App-header">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="profile" element={<Profile />} />
+          <Route path="help" element={<Help />} />
+        </Routes>
+
+        {/* 
+        {status === 'authenticated' ?  (
+        <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="profile" element={<Profile />} />
             <Route path="help" element={<Help />} />
           </Routes>
-        </header>
+          ) : null} */}
+      </header>
     </div>
   );
 }
 
 export default App;
-
